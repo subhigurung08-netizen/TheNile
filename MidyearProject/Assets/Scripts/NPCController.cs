584055faafd4be567da912b8d7869d840e4004c6
subhigurung08-netizen <subhigurung08@gmail.com>
2025-12-17 14:25:17 -0500

    editing npc and others

--- MidyearProject/Assets/Scripts/NPCController.cs
+++ MidyearProject/Assets/Scripts/NPCController.cs
@@ -7,7 +7,6 @@ public class NPCController : MonoBehaviour
 {
     private List<string> states;
    
- 
     // Start is called before the first frame update
  
     void Start()
@@ -17,15 +16,39 @@ public class NPCController : MonoBehaviour
         states.Add("Roam");
         states.Add("Chase");
         states.Add("Enchantment");
+        
     }
 
-    // Update is called once per frame
+    public Transform player;
+    // public float patrolSpeed = 10f;
+    // public float fleeSpeed = 20f;
+    public float detectionDistance = 20f;
+    // public float loseSightDistance = 20f;
+    // public float patrolRadius = 500f;
+
+    // private Vector3 patrolTarget;
+    // private bool fleeing = false;
+    public LayerMask ground;
+    
+
     void Update()
     {
-        
-        int num = Random.Range(0, states.Count);
-        Debug.Log(states[num]);
+        if(player!=null)
+        {
+            Vector3 directionToPlayer = player.position - transform.position;
+            float distanceToPlayer = directionToPlayer.magnitude;
+
+            Ray ray = new Ray(transform.position + Vector3.up * 0.5f, directionToPlayer.normalized);
+            RaycastHit hit;
+            bool playerInSight = false;
+            if (Physics.Raycast(ray, out hit, detectionDistance))
+        {
+            if (hit.transform == player)
+            {
+                playerInSight = true;
+            }
+        }
+
+        }
     }
 }
-
-  
