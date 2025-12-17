82968e570f42715cdd43bd3593e33d272d064270
subhigurung08-netizen <subhigurung08@gmail.com>
2025-12-16 23:03:12 -0500

    Default Player, basic scripts, scene name, and other basics

---
+++ MidyearProject/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject target;
+    public float offsetZ;
+    public float offsetY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(target.transform.position.x, target.transform.position.y + offsetY, target.transform.position.z + offsetZ);
+        transform.position = movement;
+    }
+}
---
+++ MidyearProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(transform.position.x + moveX * speed, 1f, transform.position.z + moveZ * speed);
+        transform.position = movement;
+
+    }
+}
