74ec41e943de24885306488ec7d0f1439340b18b
subhigurung08-netizen <subhigurung08@gmail.com>
2025-12-20 15:47:23 -0500

    trying to make spawning work

--- MidyearProject/Assets/Scripts/MenuAndSpawner.cs
+++ MidyearProject/Assets/Scripts/MenuAndSpawner.cs
@@ -2,19 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class MenuAndSpawner : MonoBehaviour
+public class Menu : MonoBehaviour
 {
     public GameObject[] spawned;
 
     // Called when we click the "Play" button.
     public void OnPlayButton()
     {
-        SceneManager.LoadScene(1);
+        PlaySpawn();
+        // SceneManager.LoadScene(1);
+        // Vector3 positionSpawned = new Vector3(Random.Range(0,100), Random.Range(5,20), Random.Range(0,100));
+        // for(int i=0; i<spawned.Length; i++){
+        //     Instantiate(spawned[i], positionSpawned, Quaternion.identity);
+        // }
+
+    }
+
+    void PlaySpawn()
+    {
+        
         Vector3 positionSpawned = new Vector3(Random.Range(0,100), Random.Range(5,20), Random.Range(0,100));
         for(int i=0; i<spawned.Length; i++){
             Instantiate(spawned[i], positionSpawned, Quaternion.identity);
         }
-
+        Debug.Log("spawned");
+        SceneManager.LoadScene(1);
     }
     // Called when we click the "Quit" button.
     public void OnQuitButton()
