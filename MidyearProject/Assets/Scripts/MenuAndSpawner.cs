eb6d9a09f2f24039749c9173db6d59ee0cc85d0c
subhigurung08-netizen <subhigurung08@gmail.com>
2025-12-30 14:10:17 -0500

    some previous edits

--- MidyearProject/Assets/Scripts/MenuAndSpawner.cs
+++ MidyearProject/Assets/Scripts/MenuAndSpawner.cs
@@ -9,25 +9,24 @@ public class Menu : MonoBehaviour
     // Called when we click the "Play" button.
     public void OnPlayButton()
     {
-        PlaySpawn();
-        // SceneManager.LoadScene(1);
-        // Vector3 positionSpawned = new Vector3(Random.Range(0,100), Random.Range(5,20), Random.Range(0,100));
-        // for(int i=0; i<spawned.Length; i++){
-        //     Instantiate(spawned[i], positionSpawned, Quaternion.identity);
-        // }
-
-    }
-
-    void PlaySpawn()
-    {
-        
-        Vector3 positionSpawned = new Vector3(Random.Range(0,100), Random.Range(5,20), Random.Range(0,100));
-        for(int i=0; i<spawned.Length; i++){
-            Instantiate(spawned[i], positionSpawned, Quaternion.identity);
-        }
-        Debug.Log("spawned");
         SceneManager.LoadScene(1);
+        for(int i=0; i<spawned.Length; i++)
+        {
+        Vector3 positionSpawned = new Vector3(0, 0, 0);
+        Instantiate(spawned[i], positionSpawned, Quaternion.identity);
+        }
     }
+
+    // void PlaySpawn()
+    // {
+        
+    //     Vector3 positionSpawned = new Vector3(Random.Range(0,100), Random.Range(5,20), Random.Range(0,100));
+    //     for(int i=0; i<spawned.Length; i++){
+    //         Instantiate(spawned[i], positionSpawned, Quaternion.identity);
+    //     }
+    //     Debug.Log("spawned");
+    //     SceneManager.LoadScene(1);
+    // }
     // Called when we click the "Quit" button.
     public void OnQuitButton()
     {
---
+++ MidyearProject/Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject spawned;
+    public int max=10;
+    public int count;
+    public LayerMask groundLayer;
+   
+    void Start()
+    {
+       count =0;
+    }
+
+    void Update()
+    {
+
+        if(count<max){
+            // int randomIndex = Random.Range(0, spawned.Length);
+            Vector3 positionSpawned = new Vector3(Random.Range(0,100), Random.Range(5,20), Random.Range(0,100));
+            Instantiate(spawned, positionSpawned, Quaternion.identity);
+            count++;
+        }
+}
+}
