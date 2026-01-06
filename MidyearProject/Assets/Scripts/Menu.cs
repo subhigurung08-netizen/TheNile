6e8f7b20f95b8d214340d3f807e4df4f17be5b61
subhigurung08-netizen <subhigurung08@gmail.com>
2026-01-06 01:04:38 -0500

    player winning added and player death through more npcs added plus other edits

--- MidyearProject/Assets/Scripts/Menu.cs
+++ MidyearProject/Assets/Scripts/Menu.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class MenuAndSpawner : MonoBehaviour
+public class Menu : MonoBehaviour
 {
-    [SerializeField] private GameObject[] spawned;
+    [SerializeField] private GameObject spawned;
+    [SerializeField] private Vector3 initialPosition;
 
     // Called when we click the "Play" button.
     public void OnPlayButton()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("DenialIsARiverInEgypt");
         // Vector3 positionSpawned = new Vector3(Random.Range(0,100), Random.Range(5,20), Random.Range(0,100));
         // for(int i=0; i<spawned.Length; i++){
         //     Instantiate(spawned[i], positionSpawned, Quaternion.identity);
         // }
+        
 
     }
     // Called when we click the "Quit" button.
--- MidyearProject/Assets/Scripts/WinExit.cs
+++ MidyearProject/Assets/Scripts/WinExit.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NavMeshPathfinding : MonoBehaviour
+public class Exit : MonoBehaviour
 {
-    [SerializeField] private Vector3 goal;
-    [SerializeField] private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<UnityEngine.AI.NavMeshAgent>().destination = goal;
+        
     }
 
     // Update is called once per frame
@@ -18,14 +16,11 @@ public class NavMeshPathfinding : MonoBehaviour
     {
         
     }
-
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag=="Player")
         {
-            Destroy(player, 1f);
-            // SceneManager.LoadScene(1);
-            SceneManager.LoadScene(2);
+           SceneManager.LoadScene(3);
         }
     }
 }
