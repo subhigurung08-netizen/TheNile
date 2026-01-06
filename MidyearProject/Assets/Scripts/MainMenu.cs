e1aeab81bc7fd45a37f6e66173e17fb1e2792a85
ode2atelier <odesinobscurity@gmail.com>
2026-01-06 00:28:42 -0500

    added win menu and lose menu, adjusted scene manager

--- MidyearProject/Assets/Scripts/MainMenu.cs
+++ MidyearProject/Assets/Scripts/MainMenu.cs
@@ -7,6 +7,6 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(1); // make sure that in build settings, the main game is index 1 or this will like. not work -w-
     }
 }
